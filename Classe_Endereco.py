7a1b65698a4c00319a1f0f92b25351ab983bc027
GilmarioNeto <150280825+GilmarioNeto@users.noreply.github.com>
2024-01-18 08:34:49 +0000

    Classe secundária
    Serve para a associação

---
+++ Classe_Endereco.py
@@ -0,0 +1,9 @@
+class Endereco():
+    def __init__(self, rua, casa, municipio, provincia, pais, bi, nif):
+        self.rua = rua
+        self.casa = casa
+        self.municipio = municipio
+        self.provincia = provincia
+        self.pais = pais
+        self.bi = bi
+        self.nif = nif
